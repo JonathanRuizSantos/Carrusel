ca10d94aa1a7e03b23dd60083d240f7945042bdc
Jonathan Ruiz Santos <jruizs1800@alumno.ipn.mx>
2022-06-06 19:45:05 -0500

    Carrusel

---
+++ XFnavegacionCarruselx1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFnavegacionCarruselx1
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btgato(object sender, EventArgs e)
+        {
+            await DisplayAlert("Aml\n Gato\n Carrusel", "ASIME\n ICE", "OK");
+        }
+
+        private async void btgallo(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageAml());
+        }
+
+        private async void btganso(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageArMaLe());
+        }
+
+        private async void btalas(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PageAlas());
+        }
+    }
+}
